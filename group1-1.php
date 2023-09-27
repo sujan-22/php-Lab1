9b6b41672abef443dfc9790a907b9d22bc7f7866
sujan-22 <sujanrokad44@gmail.com>
2023-09-26 22:12:00 -0400

    rotator and radar note

--- group1-1.php
+++ group1-1.php
@@ -5,63 +5,48 @@ $serial_number = $_GET['serial_number'];
         return ((strrev($serial_number) === $serial_number) && (strlen($serial_number) > 0));
     }
 
-    function ladderNote($serial_number) {
-        // Remove non-numeric characters
-        $numeric_serial_number = preg_replace("/[^0-9]/", "", $serial_number);
-    
-        $digits = str_split($numeric_serial_number);
-    
-        $length = count($digits);
-    
-        if ($length < 2) {
-            return "Not a Ladder Number";
-        }
-    
-        $increasing = true;
-        $decreasing = true;
-    
-        // Check if the sequence is strictly increasing
-        for ($i = 1; $i < $length; $i++) {
-            if ($digits[$i] <= $digits[$i - 1]) {
-                $increasing = false;
-                break;
-            }
-        }
-    
-        // Check if the sequence is strictly decreasing
-        for ($i = 1; $i < $length; $i++) {
-            if ($digits[$i] >= $digits[$i - 1]) {
-                $decreasing = false;
-                break;
-            }
-        }
-    
-        if ($increasing) {
+    function ladderNote($serialNumber) {
+        // Check for ladder up pattern (e.g., 3456789)
+        if (preg_match('/^\d{0,9}$/', $serialNumber) && $serialNumber === implode('', range(min(str_split($serialNumber)), max(str_split($serialNumber))))) {
             return "Ladder up note";
-        } elseif ($decreasing) {
-            return "Ladder down note";
-        } else {
-            // Check if it's a ladder up-down or ladder down-up pattern
-            $up_found = false;
-            $down_found = false;
-    
-            for ($i = 1; $i < $length; $i++) {
-                if ($digits[$i] > $digits[$i - 1]) {
-                    $up_found = true;
-                } elseif ($digits[$i] < $digits[$i - 1]) {
-                    $down_found = true;
-                }
-    
-                if ($up_found && $down_found) {
-                    return "Ladder Up-Down note";
-                }
-            }
-    
-            return "Not a Ladder Number";
         }
+    
+        // Check for ladder down pattern (e.g., 9876543)
+        if (preg_match('/^\d{0,9}$/', $serialNumber) && $serialNumber === implode('', range(max(str_split($serialNumber)), min(str_split($serialNumber))))) {
+            return "Ladder down note";
+        }
+    
+        // Check for ladder up-down pattern (e.g., 4567654)
+        $digits = str_split($serialNumber);
+        $length = count($digits);
+        $increasing = false;
+        $decreasing = false;
+
+        for ($i = 1; $i < $length; $i++) {
+            if ($digits[$i] > $digits[$i - 1]) {
+                $increasing = true;
+            } elseif ($digits[$i] < $digits[$i - 1]) {
+                $decreasing = true;
+            }
+
+            if ($increasing && $decreasing) {
+                return "Ladder up-down note";
+            }
+        }
+
+        // Check for ladder down-up pattern (e.g., 7654567)
+        $reversedSerialNumber = strrev($serialNumber);
+        if (
+            preg_match('/^\d{0,9}$/', $serialNumber) &&
+            preg_match('/^\d*(\d)(?:\d*\1)+\d*$/', $serialNumber) &&
+            preg_match('/^\d*(\d)(?:\d*\1)+\d*$/', $reversedSerialNumber)
+        ) {
+            return "Ladder down-up note";
+        }
+        
+        return "Not a Ladder Number";
     }
-    
-    
+   
     function solidNote($serial_number){
         $solid_digit = '1';
         $temp1 = str_split($serial_number);
@@ -73,7 +58,40 @@ $serial_number = $_GET['serial_number'];
     }
 
     function rotatorNote($serial_number){
-        
+        // Define the digits that can rotate
+        $rotatable_digits = [0, 1, 6, 8, 9];
+    
+        // Convert the serial number to an array of digits
+        $digits = str_split($serial_number);
+    
+        // Create a mapping of digits that can rotate to their rotated counterparts
+        $rotation_map = [
+            '0' => '0',
+            '1' => '1',
+            '6' => '9',
+            '8' => '8',
+            '9' => '6',
+        ];
+    
+        // Initialize the rotated serial number
+        $rotated_serial_number = '';
+    
+        // Iterate through the digits and check if they can be rotated
+        foreach ($digits as $digit) {
+            if (!in_array($digit, $rotatable_digits)) {
+                // If a non-rotatable digit is found, it's not a rotator note
+                return "Not a Rotator Note";
+            }
+            // Append the rotated digit to the rotated serial number
+            $rotated_serial_number .= $rotation_map[$digit] ?? $digit;
+        }
+    
+        // Reverse the rotated serial number and check if it's the same as the original
+        if (strrev($rotated_serial_number) === $serial_number) {
+            return "Rotator note";
+        } else {
+            return "Not a Rotator Note";
+        }
     }
     
     function binaryNote($serial_number){
@@ -95,6 +113,9 @@ $serial_number = $_GET['serial_number'];
         if (solidNote($serial_number)){
             $categories[] = "Solid note";
         }
+        if (rotatorNote($serial_number)){
+            $categories[] = "Rotator note";
+        }
 
         $ladderNoteResult = ladderNote($serial_number);
 
@@ -104,6 +125,8 @@ $serial_number = $_GET['serial_number'];
             $categories[] = "Ladder down";
         } elseif ($ladderNoteResult === "Ladder Up-Down note"){
             $categories[] = "Ladder Up-Down note";
+        } elseif ($ladderNoteResult === "Ladder Down-Up note"){
+            $categories[] = "Ladder Down-Up note";
         }
         return $categories;
         

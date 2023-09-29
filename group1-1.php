e6cd57559e443bdaa4faa7da154a3acb639f191d
sujan-22 <sujanrokad44@gmail.com>
2023-09-29 18:38:01 -0400

    final commit

--- group1-1.php
+++ group1-1.php
@@ -1,15 +1,67 @@
 <?php
-$serial_number = $_GET['serial_number'];
+// I, Sujan Rokad, 000882948, certify that this material is my original work.
+// No other person's work has been used wihtout suitable acknoledgment and I
+// have note made my work available to anyone else.
 
+/** 
+ * @author Sujan Rokad
+ * @version 202335.00
+ * @package COMP 10260 Assignment 1
+*/
+if(isset($_GET['serial_number'])){
+    $serial_number = $_GET['serial_number'];
+    if (empty($serial_number)) {
+        echo "Please enter a serial number!!";
+    }
+    else {
+        $categories = noteCategory($serial_number);
+
+        if (!empty($categories)){
+            foreach($categories as $category){
+                echo "<li>$category</li>";
+            }
+        }  
+        else {
+        echo "Uninteresting serial number";
+        }
+    }
+}
+
+    /**
+    * function to check Radar note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool true if pattern matched, false otherwise
+    */
     function radarNote($serial_number){
         return ((strrev($serial_number) === $serial_number) && (strlen($serial_number) > 0));
     }
 
+    /**
+    * function to check Solid note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool true if serial number is solid, false otherwise
+    */
+    function solidNote($serial_number){
+        $solid_digit = '1';
+        $temp1 = str_split($serial_number);
+        foreach($temp1 as $digit){
+            if ($digit !== $solid_digit){
+                return false;
+            }
+        } return !empty($serial_number);
+    }
+
+    /**
+    * function to check Ladder Up and Ladder Down note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool "Ladder up note" if ladder up pattern, "Ladder down note" if ladder down pattern
+    */
     function ladderNote($serial_number) {
         $ladderDigits = str_split($serial_number);
         $isLadderUp = true;
         $isLadderDown = true;
     
+        // loop to iterartes through the serial number
         for ($i = 0; $i < count($ladderDigits) - 1; $i++) {
             $difference = $ladderDigits[$i] - $ladderDigits[$i + 1];
             
@@ -28,53 +80,181 @@ $serial_number = $_GET['serial_number'];
             return "Ladder down note";
         } 
     }
-    
-   
-    function solidNote($serial_number){
-        $solid_digit = '1';
-        $temp1 = str_split($serial_number);
-        foreach($temp1 as $digit){
-            if ($digit !== $solid_digit){
-                return false;
+
+    /**
+    * function to check Ladder Up-Down note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool "Ladder Up-Down note" if pattern matched, false otherwise
+    */
+    function ladderUpDownNote($serial_number){
+        $original_serial_number = $serial_number;
+        $largestDigit = 0;
+
+        // loop to find the biggest digit in serial number
+        // reference: https://www.geeksforgeeks.org/largest-and-smallest-digit-of-a-number/
+        while ($serial_number){
+            $temp = $serial_number % 10;
+            $largestDigit = max($temp, $largestDigit);
+            $serial_number = intval($serial_number / 10);
+        }
+
+        $serialNumber_String = strval($original_serial_number); // reference: https://www.php.net/manual/en/function.strval.php
+        $indexOfLargest = strpos($serialNumber_String, $largestDigit); // reference: https://www.php.net/manual/en/function.strpos.php
+
+        $isValidUp = false;
+        $isValidDown = false;
+
+        // check for ladder up pattern
+        for ($i = 0; $i < $indexOfLargest; $i++){
+            if (intval($serialNumber_String[$i]) + 1 === intval($serialNumber_String[$i + 1]) && 
+                $serialNumber_String[$i] !== $serialNumber_String[$i + 1]){
+                $isValidUp = true;
+                break;
             }
-        } return !empty($serial_number);
+        }
+
+        // check for ladder down pattern
+        for ($i = $indexOfLargest; $i < strlen($serialNumber_String) - 1; $i++){
+            if (intval($serialNumber_String[$i]) - 1 === intval($serialNumber_String[$i + 1]) && 
+                $serialNumber_String[$i] !== $serialNumber_String[$i + 1]){
+                $isValidDown = true;
+                break;
+            }
+        }
+
+        // loop to check if two consecutive digits are same and if found it returns false
+        for ($i = 0; $i < strlen($serialNumber_String) - 1; $i++){
+            if ($serialNumber_String[$i] === $serialNumber_String[$i + 1]) {
+                return false; // not a ladder note
+            }
+        }
+
+        if ($isValidUp && $isValidDown) {
+            return "Ladder Up-Down note";
+        }
     }
 
+    /**
+    * function to check Ladder Down-Up note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool "Ladder Down-Up note" if pattern matched, false otherwise
+    */
+    function ladderDownUpNote($serial_number){
+        $original_serial_number = $serial_number;
+        $smallestDigit = 9;
+    
+        while ($serial_number){
+            $temp = $serial_number % 10;
+            $smallestDigit = min($temp, $smallestDigit);
+            $serial_number = intval($serial_number / 10);
+        }
+    
+        $serialNumber_String = strval($original_serial_number);
+        $indexOfSmallest = strpos($serialNumber_String, $smallestDigit);
+    
+        $isValidUp = false;
+        $isValidDown = false;
+    
+        // check for ladder down pattern
+        for ($i = 0; $i < $indexOfSmallest; $i++){
+            if (intval($serialNumber_String[$i]) - 1 === intval($serialNumber_String[$i + 1]) && 
+                $serialNumber_String[$i] !== $serialNumber_String[$i + 1]){
+                $isValidDown = true;
+                break;
+            }
+        }
+    
+        // check for ladder up pattern
+        for ($i = $indexOfSmallest; $i < strlen($serialNumber_String) - 1; $i++){
+            if (intval($serialNumber_String[$i]) + 1 === intval($serialNumber_String[$i + 1]) && 
+                $serialNumber_String[$i] !== $serialNumber_String[$i + 1]){
+                $isValidUp = true;
+                break;
+            }
+        }
+    
+        // loop to check if two consecutive digits are same and if found it returns false
+        for ($i = 0; $i < strlen($serialNumber_String) - 1; $i++){
+            if ($serialNumber_String[$i] === $serialNumber_String[$i + 1]) {
+                return false; // not a ladder note
+            }
+        }
+    
+        if ($isValidUp && $isValidDown) {
+            return "Ladder Down-Up note";
+        }
+    }
+    
+    /**
+    * function to check Rotator note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool "Rotator Note" if pattern found, false if not
+    */
     function rotatorNote($serial_number){
-        // Define the digits that can rotate
-        $rotatable_digits = ['0', '1', '6', '8', '9'];
-
-        for ($i=0; $i < strlen($serial_number) / 2; $i++) { 
-            $first_digit = $serial_number[$i];
-            $last_digit = $serial_number[strlen($serial_number) - 1 - $i];
-
-            if ($first_digit === '6' && $last_digit === '9'){
-                return true;
-            } if ($first_digit === '9' && $last_digit === '6'){
-                return true;
-            } if ($first_digit === '0' && $last_digit === '0'){
-                return true;
-            } if ($first_digit === '1' && $last_digit === '1'){
-                return true;
-            } if ($first_digit === '8' && $last_digit === '8'){
-                return true;
+        $serialNumber_String = strval($serial_number);
+    
+        // define the rotation map
+        $rotationMap = [
+            '0' => '0',
+            '1' => '1',
+            '6' => '9',
+            '8' => '8',
+            '9' => '6',
+        ];
+    
+        $reversedSerial = '';
+    
+        // reverse the serial number
+        for ($i = strlen($serialNumber_String) - 1; $i >= 0; $i--){
+            $digit = $serialNumber_String[$i];
+            if (!isset($rotationMap[$digit])) {
+                return false; // not a valid rotator note
             }
-        } 
+            $reversedSerial .= $rotationMap[$digit];
+        }
+    
+        //check if the reversed serial number is the same as the original
+        if ($reversedSerial === $serialNumber_String) {
+            return "Rotator Note";
+        } else {
+            return false;
+        }
     }
     
+    /**
+    * function to check Binary note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return bool true if pattern matched, false otherwise
+    */
     function binaryNote($serial_number){
         $binary_digit = [0,1];
-        $temp3 = str_split($serial_number);
-        return ((empty(array_diff($temp3,  $binary_digit))) && strlen($serial_number) > 0);
+        $temp = str_split($serial_number);
+        return ((empty(array_diff($temp,  $binary_digit))) && strlen($serial_number) > 0); // reference: https://www.php.net/manual/en/function.array-diff.php
     }
 
+    /**
+    *function to check Radar note
+    * @param string $serial_number The user entered serial number to check pattern
+    * @return array An array containing categories of all the patterns that have been found
+    */
+    // function to store and print results
     function noteCategory($serial_number){
 
-        $categories = [];
-        $serial_number = substr($serial_number, 3);
+        $categories = []; //stores returned from functions
+        $serial_number = substr($serial_number, 3); //ignores first three letters
+
         if (binaryNote($serial_number)){
             $categories[] = "Binary note";
         }
+
+        if (ladderUpDownNote($serial_number)){
+            $categories[] = "Ladder Up-Down note";
+        }
+
+        if (ladderDownUpNote($serial_number)){
+            $categories[] = "Ladder Down-Up note";  
+        }
+
         if (solidNote($serial_number)){
             $categories[] = "Solid note";
         } elseif (radarNote($serial_number)){
@@ -87,36 +267,13 @@ $serial_number = $_GET['serial_number'];
             $categories[] = "Ladder up";
         } elseif ($ladderNoteResult === "Ladder down note"){
             $categories[] = "Ladder down";
-        } elseif ($ladderNoteResult === "Ladder Up-Down note"){
-            $categories[] = "Ladder Up-Down note";
-        } elseif ($ladderNoteResult === "Ladder Down-Up note"){
-            $categories[] = "Ladder Down-Up note";
         }
+
         if (rotatorNote($serial_number)){
             $categories[] = "Rotator note";
         }
         return $categories;
         
     }
-
-    if (isset($_GET['serial_number'])) {
-        $serial_number = $_GET['serial_number'];
-        $categories = noteCategory($serial_number);
-        if (empty($serial_number)) {
-            echo "Please enter a serial number!!";
-        }
-        else {
-            if (!empty($categories)){
-                foreach($categories as $category){
-                    echo "<li>$category</li>";
-                }
-            }  
-            else {
-            echo "<ul>";
-            echo "<li>Uninteresting serial number</li>";
-            echo "</ul>";
-            }
-        }
-    }
-
+    
 ?>
--- group2-1.php
+++ group2-1.php
@@ -1,19 +1,24 @@
 <?php
-// Get the number of rows from the query string
+// get the number of rows from the query string
 $rows = $_GET['rows'];
 
+/**
+ * function to generate an HTML table row containing two columns. 
+ * @param int $rows The number of rows in the table
+ * @return string The html table row that contains two columns
+ */
 function generateTableRow($rows) {
-    // Initialize variables to store the values for the two columns
+    // initialize variables to store the values for the two columns
     $column1Value = '';
     $column2Value = 0;
 
-    // Generate the first column value (pyramid)
+    // generate the first column value (pyramid)
     for ($i = 1; $i <= $rows; $i++) {
         $column1Value .= $rows;
         $column2Value += $rows;
     }
 
-    // Build the table row HTML
+    // build the table row HTML
     $tableRow = "<tr style = 'border: 2px solid black'>
                 <td style = 'border: 2px solid black'>$column1Value</td><td style = 'border: 2px solid black'>$column2Value</td>
                 </tr>";
@@ -21,14 +26,14 @@ function generateTableRow($rows) {
     return $tableRow;
 }
 
-// Start the HTML table
+// start the HTML table
 echo '<table>';
 
-// Generate and output each row of the table
+// generate and output each row of the table
 for ($row = 1; $row <= $rows; $row++) {
     echo generateTableRow($row);
 }
 
-// Close the HTML table
+// close the HTML table
 echo '</table>';
 ?>
